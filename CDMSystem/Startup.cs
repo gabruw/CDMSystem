047f72fc9b248a07aef6ef1404312cf445c26317
Snyp <36605990+gabruw@users.noreply.github.com>
2019-05-31 22:51:15 -0300

    All

--- CDMSystem/Startup.cs
+++ CDMSystem/Startup.cs
@@ -32,7 +32,7 @@ namespace CDMSystem
             });
 
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,7 +45,6 @@ namespace CDMSystem
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
